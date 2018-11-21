1e49eb0e096a071f3f4b9b8cf0cb131050da360e
txl910514 <txl910514@sina.com>
2018-11-21 23:15:03 +0800

    add

--- src/js/index.js
+++ src/js/index.js
@@ -4,14 +4,55 @@
 import _ from 'lodash';
 import '../css/style.css'
 import './bar'
+import Axios from 'axios';
+Axios.defaults.withCredentials = true
 
 var myImage = new Image();
 myImage.src = "./../img/luoti.jpg";    //背景图片  你自己本地的图片或者在线图片
 myImage.crossOrigin = 'Anonymous'
 
-myImage.onload = function(img){
+myImage.onload = function (img) {
     console.log(img)
 }
+
+var data = function () {
+    return new Promise(function (resolve, reject) {
+        // Axios.get('http://47.52.128.170/facile/city/getCity').then(function (data) {
+        //     resolve(data)
+        // })
+        // Axios.get('http://127.0.0.1:7001/facile/city/getCity').then(function (data) {
+        //     console.log(data);
+        //     resolve(data);
+        // })
+        Axios.get('http://127.0.0.1:7001/facile/validateCode/getImageCode?type=2').then(function (data) {
+            console.log(data.data);
+            $('#imgageCode').html(data.data.data);
+            resolve(data);
+        })
+    })
+}
+
+data()
+console.log($);
+$(function () {
+    $('#imgageCode').on('click', function () {
+        Axios.get('http://127.0.0.1:7001/facile/validateCode/getImageCode?type=2').then(function (data) {
+            $('#imgageCode').html(data.data.data);
+        })
+    })
+    $('#reg').on('click', function () {
+        var params = {
+            name: $('#name').val(),
+            phone:  $('#phone').val(),
+            password:  $('#password').val(),
+            confirmPassword: $('#confirmPassword').val(),
+            code: $('#code').val()
+        }
+        Axios.post('http://127.0.0.1:7001/facile/user/createUser', params).then(function (data) {
+           console.log(data.data.msg);
+        })
+    })
+})
 
 
 
--- webpack.config.js
+++ webpack.config.js
@@ -5,11 +5,11 @@ var path = require('path');
 var webpack = require("webpack");
 let HtmlWebpackPlugin = require('html-webpack-plugin');
 const utils = {
-    assetsPath:function (_path) {
+    assetsPath: function (_path) {
         return path.posix.join('static', _path)
     }
 }
-function resolve (dir) {
+function resolve(dir) {
     return path.join(__dirname, '..', dir)
 }
 var pngUse = [
@@ -22,51 +22,56 @@ var pngUse = [
     }
 ]
 module.exports = {
-  entry: {
-    index: './src/js/index.js',
-      merge: './src/js/merge.js'
-  },
-  output: {
-    filename: '[name].js',
-    path: path.resolve(__dirname, 'dist')
-  },
+    entry: {
+        index: './src/js/index.js',
+        merge: './src/js/merge.js'
+    },
+    output: {
+        filename: '[name].js',
+        path: path.resolve(__dirname, 'dist')
+    },
     resolve: {
         extensions: ['.js', '.vue', '.json'],
         alias: {
             '@': resolve('src')
         }
     },
-  module: {
-    rules: [
-      {
-        test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                exclude: /(node_modules|bower_components)/,
+                use: pngUse
+            }
         ]
-      },
-        {
-            test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
-            exclude: /(node_modules|bower_components)/,
-            use: pngUse
-        }
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: 'jquery',
+            jQuery: 'jquery',
+        }),
+
+        new webpack.HotModuleReplacementPlugin(),
+        new HtmlWebpackPlugin({
+            title: "index",
+            filename: 'index.html',
+            template: path.resolve(__dirname, './index.html'),
+            chunks: ['index', 'vendor'],
+            inject: true
+        }),
+        new HtmlWebpackPlugin({
+            title: "merge",
+            filename: 'merge_img.html',
+            template: path.resolve(__dirname, './merge_img.html'),
+            chunks: ['merge', 'vendor'],
+            inject: true
+        }),
     ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-      new HtmlWebpackPlugin({
-          title:"index",
-          filename: 'index.html',
-          template: path.resolve(__dirname, './index.html'),
-          chunks: ['index', 'vendor'],
-          inject: true
-      }),
-      new HtmlWebpackPlugin({
-          title:"merge",
-          filename: 'merge_img.html',
-          template: path.resolve(__dirname, './merge_img.html'),
-          chunks: ['merge', 'vendor'],
-          inject: true
-      }),
-  ]
 };
